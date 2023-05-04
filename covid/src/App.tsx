f768406b8c70bfdd5298c04372890edc165ec774
khatiachub <101997849+khatiachub@users.noreply.github.com>
2023-05-04 23:45:04 +0400

    add private page

---
+++ covid/src/App.tsx
@@ -0,0 +1,28 @@
+import { Reset } from "styled-reset"
+import Questionary from "./components/Questionary"
+import { HashRouter,Routes,Route } from "react-router-dom"
+import Private from "./components/Private"
+import CovidStatus from "./components/CovidStatus"
+import Vaccination from "./components/Vaccionation"
+import CovidPolice from "./components/CovidPolice"
+import Gratitude from "./components/Gratitude"
+
+ function App() {
+  return (
+      <div>
+        <Reset/>
+        <HashRouter>
+          <Routes>
+            <Route path="/"element={<Questionary/>}/>
+            <Route path="/private"element={<Private/>}/>
+            <Route path="/covidstatus"element={<CovidStatus/>}/>
+            <Route path="/vaccination"element={<Vaccination/>}/>
+            <Route path="/covidpolice"element={<CovidPolice/>}/>
+            <Route path="/gratitude"element={<Gratitude/>}/>
+          </Routes>
+        </HashRouter>
+      </div>
+  )
+}
+
+export default App
